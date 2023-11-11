8fbd02407368a9085129a8755c8282f69b8dd85e
1Taafe <dima759mi@gmail.com>
2023-11-11 09:42:33 +0300

    Initial Commit

---
+++ QueryQuest/QueryQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueryQuestApp.swift
+//  QueryQuest
+//
+//  Created by Дмитрий Заянковский on 11.11.23.
+//
+
+import SwiftUI
+
+@main
+struct QueryQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
